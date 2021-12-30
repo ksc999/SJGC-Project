8ca3b8a0f241f8f2021303d5c0afa0f3fa678456
ksc999 <ksc19@mails.tsinghua.edu.cn>
2021-12-30 09:35:08 +0800

    tmp

--- my_gpr.py
+++ my_gpr.py
@@ -2,11 +2,18 @@ import sys
 import pandas as pd
 import numpy as np
 from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 
 
 class MyGPR:
-    def __init__(self, data_path, train_ratio=0.5, selection_mode='trunctated', kernel_name='rbf',
-                 fit_param_times=1000):
+    def __init__(
+            self, data_path,
+            train_ratio=0.9,
+            selection_mode='trunctated',
+            kernel_name='rbf',
+            fit_param_times=10,
+            log_sample_flag=True
+        ):
         data = pd.read_csv(data_path)
         money = data['Close/Last']
         money = list(map(lambda str_money: float(str_money[1:]), money))    # convert str to float
@@ -19,6 +26,7 @@ class MyGPR:
         self.selection_mode = selection_mode
         self.kernel_name = kernel_name
         self.fit_param_times = fit_param_times
+        self.log_sample_flag = log_sample_flag
         self._divide_data()
         self.kernel_param, self.kernel_param_bound = self._get_kernel_params()
         self.kernel = self._get_kernel()
@@ -31,19 +39,21 @@ class MyGPR:
             self.x_train = self.x[:dividing_line]
             self.x_test = self.x[dividing_line:]
             self.y_train = self.y[:dividing_line]
-            self.y_test = self.y[:dividing_line]
+            self.y_test = self.y[dividing_line:]
         # 'random' means that GPR is taken as a information completion task
         elif self.selection_mode == 'random':
             train_nums = int(len(self.x) * self.train_ratio)
             self.x_train = np.random.choice(self.x, size=train_nums, replace=False)
             self.x_test = np.setdiff1d(self.x, self.x_train)
             self.y_train = self.y[self.x_train]
-            self.y_test = self.y[self.y_test]
+            self.y_test = self.y[self.x_test]
     
     def _get_kernel_params(self):
         if self.kernel_name == 'rbf':
-            param = {'l': 100, 'sigma_f': 20, 'sigma_n': 1e-2}
-            param_bound = {'l': (1e-4, 1e2), 'sigma_f': (1e-4, 1e2), 'sigma_n': (1e-10, 1e-2)}
+            # param = {'l': 6.2949, 'sigma_f': 70.6718, 'sigma_n': 0.1}
+            # param_bound = {'l': (1e-1, 1e2), 'sigma_f': (1e-2, 1e2), 'sigma_n': (1e-10, 1e-3)}
+            param = {'l': 1.1187343193934338, 'sigma_f': 16.20733936502278}
+            param_bound = {'l': (1e-1, 1e3), 'sigma_f': (1e-1, 1e2)}
             
         return param, param_bound    
             
@@ -51,7 +61,7 @@ class MyGPR:
         if self.kernel_name == 'rbf':   
             def kernel(param, x_1, x_2):
                 # the distance matrix is of the size (x_1, x_2)
-                distance_matrix = np.sum(x_1**2).reshape(-1, 1) + np.sum(x_2**2) \
+                distance_matrix = np.power(x_1, 2).reshape(-1, 1) + np.power(x_2, 2) \
                                     - 2 * np.dot(x_1.reshape(-1, 1), x_2.reshape(1, -1))
                 return param[1]**2 * np.exp(-0.5 / param[0]**2 * distance_matrix)
             
@@ -60,11 +70,8 @@ class MyGPR:
     def _get_kernel_loss(self):
         if self.kernel_name == 'rbf':
             def kernel_loss(param, x, y):
-                K = self.kernel(param, x, x) \
-                        + param[2] * np.eye(len(x))
-                loss = 0.5 * np.dot(y, np.linalg.inv(K) @ y) \
-                        + 0.5 * np.linalg.slogdet(K)[1] \
-                        + 0.5 * len(x) * np.log(2 * np.pi)
+                K = self.kernel(param, x, x) + 1e-8 * np.eye(len(x))
+                loss = 0.5 * np.dot(y, np.linalg.inv(K) @ y) + 0.5 * np.linalg.slogdet(K)[1] 
                 return loss.ravel()
             
         return kernel_loss
@@ -74,13 +81,16 @@ class MyGPR:
         # we have to try many initial values of the args to get a pretty good result
         args_init_list = []
         for lower_bound, upper_bound in self.kernel_param_bound.values():
-            # use log interpolation
-            log_lower_bound = np.log(lower_bound)
-            log_upper_bound = np.log(upper_bound)
-            log_list = np.linspace(log_lower_bound, log_upper_bound, self.fit_param_times)
-            exp_list = np.exp(log_list)
-            np.random.shuffle(exp_list) # shuffle the list
-            args_init_list.append(exp_list)
+            if self.log_sample_flag:
+                # use log interpolation
+                log_lower_bound = np.log(lower_bound)
+                log_upper_bound = np.log(upper_bound)
+                log_list = np.linspace(log_lower_bound, log_upper_bound, self.fit_param_times)
+                tmp_list = np.exp(log_list)
+            else:
+                tmp_list = np.linspace(lower_bound, upper_bound, self.fit_param_times)
+            np.random.shuffle(tmp_list) # shuffle the list
+            args_init_list.append(tmp_list)
         args_init_list = np.array(args_init_list)
         args_bound = tuple(self.kernel_param_bound.values())
         fun_min = sys.float_info.max
@@ -95,9 +105,45 @@ class MyGPR:
             self.kernel_param[key] = optimal_result[i]
         print(f'kernel parameters after optimize: {self.kernel_param}')
         print(f'loss value now: {fun_min}')
+
+    def predict(self):
+        param = list(self.kernel_param.values())
+        K_train_train = self.kernel(param, self.x_train, self.x_train)
+        K_test_train = self.kernel(param, self.x_test, self.x_train)
+        K_test_test = self.kernel(param, self.x_test, self.x_test)
+        self.mu_test = K_test_train @ np.linalg.inv(K_train_train) @ self.y_train
+        self.cov_test = K_test_test - K_test_train @ np.linalg.inv(K_train_train) @ K_test_train.T
     
+    def draw_prediction(self, img_name, prefix='./imgs/', fig_size=(20, 10)):
+        var = np.diag(self.cov_test)
+        CI_upper_bound_test = self.mu_test + 1.96 * np.sqrt(var)
+        CI_lower_bound_test = self.mu_test - 1.96 * np.sqrt(var)
+        CI_upper_bound = np.zeros(len(self.x))
+        CI_upper_bound[self.x_test] = CI_upper_bound_test
+        CI_upper_bound[self.x_train] = self.y_train
+        CI_lower_bound = np.zeros(len(self.x))
+        CI_lower_bound[self.x_test] = CI_lower_bound_test
+        CI_lower_bound[self.x_train] = self.y_train
+        plt.figure(figsize=fig_size,dpi=90)
+        if self.selection_mode == 'trunctated':
+            plt.plot(self.x_train, self.y_train, 'k', label='train values')
+            plt.plot(np.insert(self.x_test, 0, self.x_train[-1]), 
+                     np.insert(self.y_test, 0, self.y_train[-1]), 
+                     'r--', label='true test values')
+            plt.plot(np.insert(self.x_test, 0, self.x_train[-1]), 
+                     np.insert(self.mu_test, 0, self.y_train[-1]), 
+                     'b', label='predicted test values')
+            plt.fill_between(self.x_test, CI_lower_bound_test, CI_upper_bound_test, alpha=0.3)
+        elif self.selection_mode == 'random':
+            y = np.zeros(len(self.x))
+            y[self.x_train] = self.y_train
+            y[self.x_test] = self.mu_test
+            plt.plot(self.x, y, 'b', label='predicted values')
+            plt.plot(self.x_train, self.y_train, 'rx', label='train values', ms=8)
+            plt.plot(self.x, self.y, 'r--', label='true values', alpha=0.8)
+            plt.fill_between(self.x, CI_lower_bound, CI_upper_bound, alpha=0.3)
+        plt.legend(prop={'size': 18})
+        plt.savefig(prefix + img_name)    
 
 
-if __name__ == '__main__':
-    demo_gpr = MyGPR("./data/adsk/adsk_1M.csv")
-    demo_gpr.fit_params()
+    
